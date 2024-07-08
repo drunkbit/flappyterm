4dd345a56ebe3e9096b6e0644ff64c176e3f4c50
drunkbit <174365495+drunkbit@users.noreply.github.com>
2024-07-08 15:12:33 +0200

    optimize code

--- src/flappyterm.py
+++ src/flappyterm.py
@@ -36,11 +36,9 @@ class flappyterm:
 
     def draw(self):
         flappyterm.clear()
-        for y in range(self.height):
-            for x in range(self.width):
-                sys.stdout.write(self.yx[y][x])
-            sys.stdout.write("\n")
-        sys.stdout.write("<ESC> -> Exit / <Backspace> -> Restart / <Space> -> Jump: ")
+        for row in self.yx:
+            print("".join(row))
+        print("<ESC> -> Exit / <Backspace> -> Restart / <Space> -> Jump: ", end="")
         sys.stdout.flush()
 
     def recommendations(self):
@@ -51,34 +49,19 @@ class flappyterm:
 
     def lost(self):
         tmp = "You Lost! Score:     "
-        center_height = int(flappyterm.get_height() / 2) - 1
-        center_width = int(flappyterm.get_width() / 2) - 11
-        n = 0
+        center_height = flappyterm.get_height() // 2 - 1
+        center_width = flappyterm.get_width() // 2 - 11
         self.yx[center_height][center_width] = "┌"
-        for a in range(22):
-            self.yx[center_height][center_width + 1 + a] = "─"
+        self.yx[center_height][center_width + 1 : center_width + 23] = "─" * 22
         self.yx[center_height][center_width + 23] = "┐"
         self.yx[center_height + 1][center_width] = "│"
         self.yx[center_height + 1][center_width + 23] = "│"
         self.yx[center_height + 2][center_width] = "└"
-        for a in range(22):
-            self.yx[center_height + 2][center_width + 1 + a] = "─"
+        self.yx[center_height + 2][center_width + 1 : center_width + 23] = "─" * 22
         self.yx[center_height + 2][center_width + 23] = "┘"
-        for a in tmp:
-            self.yx[center_height + 1][center_width + 2 + n] = a
-            n = n + 1
-        if score < 10:
-            self.yx[center_height + 1][center_width + 19] = "0"
-            self.yx[center_height + 1][center_width + 20] = "0"
-            self.yx[center_height + 1][center_width + 21] = str(score)[0]
-        elif score > 9 and score < 1000:
-            self.yx[center_height + 1][center_width + 19] = "0"
-            self.yx[center_height + 1][center_width + 20] = str(score)[0]
-            self.yx[center_height + 1][center_width + 21] = str(score)[1]
-        else:
-            self.yx[center_height + 1][center_width + 19] = str(score)[0]
-            self.yx[center_height + 1][center_width + 20] = str(score)[1]
-            self.yx[center_height + 1][center_width + 21] = str(score)[2]
+        self.yx[center_height + 1][center_width + 2 : center_width + 2 + len(tmp)] = tmp
+        score_str = str(score).zfill(3)
+        self.yx[center_height + 1][center_width + 19 : center_width + 22] = score_str
 
     def play(self):
         global score
@@ -130,75 +113,49 @@ class bird:
         tmp0 = 0
         if dead == False:
             bird.fall()
-            if self.y >= 1 and self.y <= flappyterm.get_height() - 2:
-                if bird.check_collision(self.y - 1, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y - 1, self.x + 2):
-                    dead = True
-                elif bird.check_collision(self.y - 1, self.x + 3):
-                    dead = True
-                elif bird.check_collision(self.y, self.x):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 2):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 3):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 4):
-                    dead = True
-                elif bird.check_collision(self.y + 1, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y + 1, self.x + 3):
-                    dead = True
-                else:
-                    dead = False
-                flappyterm.get_yx()[self.y - 1][self.x + 1] = "_"
-                flappyterm.get_yx()[self.y - 1][self.x + 2] = "_"
-                flappyterm.get_yx()[self.y - 1][self.x + 3] = "_"
-                flappyterm.get_yx()[self.y][self.x] = "("
-                flappyterm.get_yx()[self.y][self.x + 1] = "O"
-                flappyterm.get_yx()[self.y][self.x + 2] = ","
-                flappyterm.get_yx()[self.y][self.x + 3] = "O"
-                flappyterm.get_yx()[self.y][self.x + 4] = ")"
+            if 1 <= self.y <= flappyterm.get_height() - 2:
+                collision = False
+                for i in range(self.y - 1, self.y + 2):
+                    for j in range(self.x + 1, self.x + 5):
+                        if bird.check_collision(i, j):
+                            collision = True
+                            break
+                    if collision:
+                        break
+                dead = collision
+                flappyterm.get_yx()[self.y - 1][self.x + 1 : self.x + 4] = ["_"] * 3
+                flappyterm.get_yx()[self.y][self.x : self.x + 5] = [
+                    "(",
+                    "O",
+                    ",",
+                    "O",
+                    ")",
+                ]
                 flappyterm.get_yx()[self.y + 1][self.x + 1] = '"'
                 flappyterm.get_yx()[self.y + 1][self.x + 3] = '"'
             else:
-                if not self.y >= 1:
+                if self.y < 1:
                     tmp0 = 1
-                if not self.y <= flappyterm.get_height() - 2:
+                if self.y > flappyterm.get_height() - 2:
                     tmp0 = flappyterm.get_height() - 2
                 self.y = tmp0
-                if bird.check_collision(self.y - 1, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y - 1, self.x + 2):
-                    dead = True
-                elif bird.check_collision(self.y - 1, self.x + 3):
-                    dead = True
-                elif bird.check_collision(self.y, self.x):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 2):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 3):
-                    dead = True
-                elif bird.check_collision(self.y, self.x + 4):
-                    dead = True
-                elif bird.check_collision(self.y + 1, self.x + 1):
-                    dead = True
-                elif bird.check_collision(self.y + 1, self.x + 3):
-                    dead = True
-                else:
-                    dead = False
-                flappyterm.get_yx()[self.y - 1][self.x + 1] = "_"
-                flappyterm.get_yx()[self.y - 1][self.x + 2] = "_"
-                flappyterm.get_yx()[self.y - 1][self.x + 3] = "_"
-                flappyterm.get_yx()[self.y][self.x] = "("
-                flappyterm.get_yx()[self.y][self.x + 1] = "O"
-                flappyterm.get_yx()[self.y][self.x + 2] = ","
-                flappyterm.get_yx()[self.y][self.x + 3] = "O"
-                flappyterm.get_yx()[self.y][self.x + 4] = ")"
+                collision = False
+                for i in range(self.y - 1, self.y + 2):
+                    for j in range(self.x + 1, self.x + 5):
+                        if bird.check_collision(i, j):
+                            collision = True
+                            break
+                    if collision:
+                        break
+                dead = collision
+                flappyterm.get_yx()[self.y - 1][self.x + 1 : self.x + 4] = ["_"] * 3
+                flappyterm.get_yx()[self.y][self.x : self.x + 5] = [
+                    "(",
+                    "O",
+                    ",",
+                    "O",
+                    ")",
+                ]
                 flappyterm.get_yx()[self.y + 1][self.x + 1] = '"'
                 flappyterm.get_yx()[self.y + 1][self.x + 3] = '"'
 
@@ -242,34 +199,23 @@ class pipe:
                     if self.x + j < 80:
                         flappyterm.get_yx()[i][self.x + j] = self.symbol
                         if i == self.y:
-                            flappyterm.get_yx()[self.y - 4][self.x + j] = " "
-                            flappyterm.get_yx()[self.y - 3][self.x + j] = " "
-                            flappyterm.get_yx()[self.y - 2][self.x + j] = " "
-                            flappyterm.get_yx()[self.y - 1][self.x + j] = " "
-                            flappyterm.get_yx()[self.y][self.x + j] = " "
-                            flappyterm.get_yx()[self.y + 1][self.x + j] = " "
-                            flappyterm.get_yx()[self.y + 2][self.x + j] = " "
-                            flappyterm.get_yx()[self.y + 3][self.x + j] = " "
-                            flappyterm.get_yx()[self.y + 4][self.x + j] = " "
-                            flappyterm.get_yx()[self.y + 5][self.x + j] = " "
-        self.x = self.x - 1
+                            for k in range(-4, 6):
+                                flappyterm.get_yx()[self.y + k][self.x + j] = " "
+            self.x -= 1
 
 
 def on_press(event):
-    global done
-    global dead
-    global bird
+    global done, dead, bird
     key = event.name
     if key == "esc":
         done = True
         dead = True
         return False
-    if key == "backspace":
-        if dead == True:
-            dead = False
+    if key == "backspace" and dead:
+        dead = False
     if key == "space":
         bird.jump()
-        if dead == True:
+        if dead:
             dead = False
 
 
@@ -278,7 +224,7 @@ bird = bird(10, 11, 0.05, hz * 3)
 
 if __name__ == "__main__":
     keyboard.on_press(on_press)
-    flappyterm.recommendations()
+    # flappyterm.recommendations()
     while done == False:
         while dead == False:
             flappyterm.play()
